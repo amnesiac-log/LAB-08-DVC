022b2ce8ddde373bfc2c981fe4f10a7b06ea3542
2022BCS0022-AMOGH <movies4amogh@gmail.com>
2026-03-06 11:41:43 +0530

    Add training script

---
+++ src/train.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import root_mean_squared_error, r2_score
+
+df = pd.read_csv("data/housing.csv")
+
+X = df.drop("median_house_value", axis=1)
+y = df["median_house_value"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+pred = model.predict(X_test)
+
+rmse = root_mean_squared_error(y_test, pred)
+r2 = r2_score(y_test, pred)
+
+print("RMSE:", rmse)
+print("R2:", r2)
